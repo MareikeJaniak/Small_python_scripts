fc4cba5ecb59d7149553ebc1a95699de4e67177b
MareikeJaniak <30602072+MareikeJaniak@users.noreply.github.com>
2020-01-21 15:11:32 +0100

    Add files via upload

--- find_df_overlap.py
+++ find_df_overlap.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# find genes that are annotated as pseudogenes by both PseudoPipe and SnpEff output. PseudoPipe annotated with gelada ensembl protein ID, SnpEff annotated with baboon entrezgene accession.
+
 import pandas as pd
 import numpy as np
 
@@ -7,13 +9,15 @@ pp_df = pd.read_csv('/home/mcj43/scratch/Gelada_project/PseudoPipe/gelada/ppipe_
 ID_df = pd.read_csv('/home/mcj43/scratch/Gelada_project/PseudoPipe/gelada/matched_IDs.tsv', sep='\t')
 SnpEff_df = pd.read_csv('/home/mcj43/scratch/Gelada_project/SnpEff/filteredVCF/geladas.panu3.snpEff.filtered.HIGH.STOPGAINED.NO-NON-ALT.recode.annotate.EXTRACT.vcf',sep='\t')
 
-overlap = np.intersect1d(pp_df["query"], ID_df["ProteinID"])
+pp_df["query"] = pp_df["query"].str.replace('\.1','')
 
-matched_IDs_pp = ID_df[ID_df.ProteinID.isin(overlap)]
+overlap = np.intersect1d(pp_df["query"], ID_df["Protein_ID_Tgel"])
+
+matched_IDs_pp = ID_df[ID_df.Protein_ID_Tgel.isin(overlap)]
 
 #overlap_pp_SnpEff = np.intersect1d(matched_IDs_pp["GeneSymbol"], SnpEff_df["GENE"])
 #print(overlap_pp_SnpEff)
-out = matched_IDs_pp[matched_IDs_pp["GeneSymbol"].isin(SnpEff_df["GENE"])]
+out = matched_IDs_pp[matched_IDs_pp["NCBI_gene_Panu"].isin(SnpEff_df["GENE"])]
 
-with open("/home/mcj43/scratch/Gelada_project/PseudoPipe/gelada/overlap_pp_SnpEff.tsv", "w") as outfile:
+with open("/home/mcj43/scratch/Gelada_project/PseudoPipe/gelada/Pseudogenes_overlap_PP_SnpEff.tsv", "w") as outfile:
 	out.to_csv(outfile, sep='\t', index=False)
--- match_proteinID.py
+++ match_proteinID.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-### script to extract ensembl protein id, gene id, and gene symbol from fasta header in protein sequence fasta file and write to tsv that contains three columns.
+### script to extract ensembl protein id, gene id, and gene symbol from fasta header in protein sequence fasta file and write to tsv that contains three columns. In second step, matching entrezgene accessions from the Papio anubis genome (Panu_3.0) are added as a fourth column. 
+# output used to find overlap between PseudoPipe and SnpEff results, which are annotated with different genomes.
 
 import pandas as pd
 
@@ -12,6 +13,7 @@ with open('/home/mcj43/scratch/Gelada_project/PseudoPipe/gelada/ppipe_input/pep/
 			line = line.rstrip()
 			line = line.replace('>','')
 			line = line.replace('gene:','')
+			line = line.replace('.1','')
 			if 'gene_symbol' in line:				# not all lines contain gene symbol info, leading to index out of range error
 				line = line.replace('gene_symbol:','')
 				line_list = line.split(' ')
@@ -20,12 +22,21 @@ with open('/home/mcj43/scratch/Gelada_project/PseudoPipe/gelada/ppipe_input/pep/
 				info_list.append(str(line_list[7]))
 				lst.append(info_list)
 			else:							# solved by only extracting 7th index for lines that contain gene_symbol
-				line = line.split(' ')
+				line_list = line.split(' ')
 				info_list = [str(line_list[0])]
 				info_list.append(str(line_list[3]))
 				lst.append(info_list)
+		else:
+			continue
 
-df = pd.DataFrame(lst, columns = ['ProteinID','GeneID','GeneSymbol'])
+df = pd.DataFrame(lst, columns = ['Protein_ID_Tgel','GeneID','GeneSymbol'])
 
+# add matched baboon entrezgene_accession to the df containing gelada proteinID, geneID, and geneSymbol (compiled with biomaRt)
+
+df_PanuTgel = pd.read_csv('/home/mcj43/scratch/Gelada_project/PseudoPipe/gelada/matched_baboonEntrez_geladaEnsembl.tsv', sep='\t')
+
+df_matched = pd.merge(df,df_PanuTgel, on='Protein_ID_Tgel', how='left')
+
+#print(df_matched)
 with open('/home/mcj43/scratch/Gelada_project/PseudoPipe/gelada/matched_IDs.tsv', 'w') as out_file:
-	df.to_csv(out_file, sep='\t', index=False)
+	df_matched.to_csv(out_file, sep='\t', index=False)
